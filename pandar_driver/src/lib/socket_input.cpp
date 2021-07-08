aaf4b2ecfd7bb704194f96dc4c0f6d38cf680c7c
Shinnosuke Hirakawa <shinnosuke.hirakawa@tier4.jp>
2021-07-08 14:51:36 +0900

    fix

--- pandar_driver/src/lib/socket_input.cpp
+++ pandar_driver/src/lib/socket_input.cpp
@@ -13,7 +13,7 @@ SocketInput::SocketInput(const std::string& device_ip, uint16_t port, uint16_t g
     gps_socket_(io_service_, udp::endpoint(udp::v4(), gps_port))
 {
   device_ip_ = boost::asio::ip::address::from_string(device_ip);
-  lidar_socket_.set_option(boost::asio::detail::socket_option::integer<SOL_SOCKET, SO_RCVTIMEO>(timeout));
+  // lidar_socket_.set_option(boost::asio::detail::socket_option::integer<SOL_SOCKET, SO_RCVTIMEO>(timeout));
   timeout_ = timeout;
 }
 
@@ -24,11 +24,10 @@ SocketInput::~SocketInput(void)
 
 SocketInput::PacketType SocketInput::getPacket(pandar_msgs::PandarPacket* pkt)
 {
-  // boost::array<uint8_t,ETHERNET_MTU> recv_buf;
   boost::system::error_code error_code;
   udp::endpoint remote_endpoint;
-  size_t packet_size = lidar_socket_.receive_from(boost::asio::buffer(pkt->data), remote_endpoint, 0, error_code);
 
+  size_t packet_size = lidar_socket_.receive_from(boost::asio::buffer(pkt->data, ETHERNET_MTU), remote_endpoint, 0, error_code);
   if(error_code == boost::system::errc::success && remote_endpoint.address() == device_ip_){
     pkt->stamp = ros::Time::now();
     pkt->size = static_cast<uint32_t>(packet_size);
